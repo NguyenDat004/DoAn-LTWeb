533a9903aeb5690aff70d1def8d6e23f8ff1d891
NguyenDat004 <nguyendat135ok@gmail.com>
2024-10-31 22:41:44 +0700

    luomchode

--- layout/header.php
+++ layout/header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width-device-width, initial-scale=1.0">
-        <script src="https://kit.fontawesome.com/1147679ae7.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link href="/htdocts/assets/caterory.css" rel="stylesheet">
         <link rel="icon" href="<?php echo $info['icon']; ?>" type="image/x-icon" />
         <title><?php echo $info['title']; ?></title>
